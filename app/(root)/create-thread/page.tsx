f6ab3539fc058e2f26bfb13626b15184a3c6c8d3
AU1000 <upadhyayayush21@gmail.com>
2024-07-18 20:52:38 +0530

    changed mongodb url

--- app/(root)/create-thread/page.tsx
+++ app/(root)/create-thread/page.tsx
@@ -1,12 +1,14 @@
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
+
 import PostThread from "@/components/forms/PostThread";
 import { fetchUser } from "@/lib/actions/user.actions";
 
 async function Page() {
   const user = await currentUser();
   if (!user) return null;
+  
 
   // fetch organization list created by user
   const userInfo = await fetchUser(user.id);
@@ -14,7 +16,7 @@ async function Page() {
 
   return (
     <>
-      <h1 className='head-text'>Create Thread</h1>
+      <h1 className='head-text'>CREATE THREAD</h1>
 
       <PostThread userId={userInfo._id} />
     </>
